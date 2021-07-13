770e85e85729927053ecebc214d552f928f223f9
Gabor Marton <gabor.marton@ericsson.com>
2021-07-13 17:33:50 +0200

    Fix printState-eq-classes test case

--- clang/test/Analysis/expr-inspection-printState-eq-classes.c
+++ clang/test/Analysis/expr-inspection-printState-eq-classes.c
@@ -8,14 +8,13 @@ void test_equivalence_classes(int a, int b, int c, int d) {
     return;
   if (a != d)
     return;
-  if (b != 0)
+  if (a != c)
     return;
   clang_analyzer_printState();
   (void)(a * b * c * d);
   return;
 }
 
-  // CHECK:      "equivalence_classes": [
-  // CHECK-NEXT:   [ "((reg_$0<int a>) + (reg_$1<int b>)) != (reg_$2<int c>)", "(reg_$0<int a>) != (reg_$2<int c>)" ],
-  // CHECK-NEXT:   [ "(reg_$0<int a>) + (reg_$1<int b>)", "reg_$0<int a>", "reg_$2<int c>", "reg_$3<int d>" ]
-  // CHECK-NEXT: ],
+ // CHECK:       "equivalence_classes": [
+ // CHECK-NEXT:    [ "(reg_$0<int a>) + (reg_$1<int b>)", "reg_$0<int a>", "reg_$2<int c>", "reg_$3<int d>" ]
+ // CHECK-NEXT:  ],
