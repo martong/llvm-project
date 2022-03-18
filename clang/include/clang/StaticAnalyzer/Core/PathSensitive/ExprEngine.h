053c3dd7a55a932e63da5f6b408ec0c11818a59b
Gabor Marton <gabor.marton@ericsson.com>
2022-03-18 14:22:52 +0100

    Add ctuBifurcate which wraps inlineCall

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/ExprEngine.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/ExprEngine.h
@@ -805,7 +805,10 @@ private:
                         const ExplodedNode *Pred,
                         const EvalCallOptions &CallOpts = {});
 
-  bool inlineCall(const CallEvent &Call, const Decl *D, NodeBuilder &Bldr,
+  bool inlineCall(WorkList *WList, const CallEvent &Call, const Decl *D,
+                  NodeBuilder &Bldr, ExplodedNode *Pred, ProgramStateRef State);
+
+  bool ctuBifurcate(const CallEvent &Call, const Decl *D, NodeBuilder &Bldr,
                   ExplodedNode *Pred, ProgramStateRef State);
 
   /// Conservatively evaluate call by invalidating regions and binding
