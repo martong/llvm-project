360aa1f58045c74be5d83e0061d79c4b5e257414
Gabor Marton <gabor.marton@ericsson.com>
2022-05-11 10:09:00 +0200

    Handle UnarySymExpr in SValExplainer

--- clang/test/Analysis/unary-sym-expr.c
+++ clang/test/Analysis/unary-sym-expr.c
@@ -4,8 +4,14 @@
 // RUN:   -verify
 
 void clang_analyzer_eval(int);
+void clang_analyzer_dump(int);
+void clang_analyzer_explain(int);
 
 int test(int flag) {
+
+  clang_analyzer_dump(-flag);    // expected-warning{{-reg_$0<int flag>}}
+  clang_analyzer_explain(-flag); // expected-warning{{- (argument 'flag')}}
+
   if (-flag == 0) {
     clang_analyzer_eval(-flag == 0); // expected-warning{{TRUE}}
     clang_analyzer_eval(-flag > 0);  // expected-warning{{FALSE}}
