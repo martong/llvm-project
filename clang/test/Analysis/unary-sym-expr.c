5bf3d9155c58b2df26382b3bde4404018a1e2e0d
Gabor Marton <gabor.marton@ericsson.com>
2022-05-11 17:49:36 +0200

    Add false positive test

--- clang/test/Analysis/unary-sym-expr.c
+++ clang/test/Analysis/unary-sym-expr.c
@@ -34,3 +34,12 @@ void test_svalbuilder_simplification(int x, int y) {
     return;
   clang_analyzer_eval(-(x + y) == -3); // expected-warning{{TRUE}}
 }
+
+int test_fp(int flag) {
+  int value;
+  if (flag == 0)
+    value = 1;
+  if (-flag == 0)
+    return value; // no-warning
+  return 42;
+}
