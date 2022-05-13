1af6ac1d2a9c5690ff91cf0bfb197a7e0cd358cb
Gabor Marton <gabor.marton@ericsson.com>
2022-05-13 11:01:36 +0200

    Check that the AST file is loaded in existingdef test file

--- clang/test/Analysis/ctu-onego-existingdef.cpp
+++ clang/test/Analysis/ctu-onego-existingdef.cpp
@@ -43,6 +43,17 @@ int bar() {
 // RUN:   -analyze-function='baruser(int)' -x c++ \
 // RUN:   -analyzer-config ctu-max-nodes-pct=100 \
 // RUN:   -analyzer-config ctu-max-nodes-min=1000
+//Check that the AST file is loaded.
+// RUN: %clang_analyze_cc1 -std=c++14 -triple x86_64-pc-linux-gnu \
+// RUN:   -analyzer-checker=core,debug.ExprInspection \
+// RUN:   -analyzer-config eagerly-assume=false \
+// RUN:   -analyzer-config experimental-enable-naive-ctu-analysis=true \
+// RUN:   -analyzer-config ctu-dir=%t/ctudir \
+// RUN:   -analyze-function='baruser(int)' -x c++ \
+// RUN:   -analyzer-config ctu-max-nodes-pct=100 \
+// RUN:   -analyzer-config display-ctu-progress=true \
+// RUN:   -analyzer-config ctu-max-nodes-min=1000 2>&1 %s | FileCheck %s
+// CHECK: CTU loaded AST file
 
 void other(); // Defined in the other TU.
 
